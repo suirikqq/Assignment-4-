033b86aa465ce6585b05520374116284e5ffa963
suirikqq <145866061+suirikqq@users.noreply.github.com>
2023-11-03 01:13:49 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js
+
+// Get a reference to the "Learn more" button using its class name
+const learnMoreButton = document.querySelector('.btn-primary');
+
+// Add a click event listener to the button
+learnMoreButton.addEventListener('click', function() {
+    // Your code to handle the button click event goes here
+    alert("This button doesn't work!");
+});
